15e0ae9092fce5474a6873390ca75f9bbe10c9de
ahmedyar7 <ahmedyar778@gmail.com>
2026-07-12 16:31:38 +0500

    Implement cache loading and refreshing mechanism for attack data

--- server/main.py
+++ server/main.py
@@ -12,6 +12,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import json
+import time
+from pathlib import Path
+
+# --- Saving the Cache --- #
+
+CACHE_FILE = Path("./data/attack_cache.json")
+CACHE_MAX_AGE = 6 * 60 * 60  # 6 hrs in sec format
+
+# --- Environment Variables --- #
 
 ABUSEIPDB_KEY = os.environ["ABUSEIPDB_KEY"]
 DB_PATH = "./data/GeoLite2-City.mmdb"
@@ -62,7 +72,33 @@ def refresh_attack():
             )
 
     attack_cache = results
+    CACHE_FILE.write_text(
+        json.dumps(results)
+    )  # This would persist after the success...
     print(f"Cache Refreshed: {len(attack_cache)} plottable IP(s)")
+
+
+def load_cache_or_fetch():
+    """
+    At Startup:
+    1. Use the disk cache if it's fresh.
+    2. Only hit the API if it's stale/missing
+    """
+
+    global attack_cache
+
+    if CACHE_FILE.exists():
+        age = time.time() - CACHE_FILE.stat().st_mtime
+
+        if age < CACHE_MAX_AGE:
+            attack_cache = json.loads(CACHE_FILE.read_text())
+
+            print(
+                f"Loaded {len(attack_cache)} IPs from disk cache ({age/60:.0f}) min old"
+            )
+            return
+
+    refresh_attack()
 
 
 @asynccontextmanager
@@ -70,19 +106,20 @@ async def lifespan(app: FastAPI):
     refresh_attack()
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(refresh_attack,"interval",minutes=10)
+    scheduler.add_job(refresh_attack, "interval", minutes=10)
     scheduler.start()
 
     yield
     scheduler.shutdown()
 
-app = FastAPI(title="NetFlare",lifespan=lifespan)
+
+app = FastAPI(title="NetFlare", lifespan=lifespan)
+
 
 @app.get("/")
 def home():
-    return {
-        "msg" : "NetFlare Server side is running fine"
-    }
+    return {"msg": "NetFlare Server side is running fine"}
+
 
 @app.get("/attacks")
 def get_attacks():
@@ -90,4 +127,4 @@ def get_attacks():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True) 
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
