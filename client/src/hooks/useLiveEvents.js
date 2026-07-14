fb0aef4b52f70f68b0179c199a7f66fa99345292
ahmedyar7 <ahmedyar778@gmail.com>
2026-07-14 19:13:45 +0500

    Creating the useLiveEffect hook get the live threat data and then parse them into json and the close the web-socket

---
+++ client/src/hooks/useLiveEvents.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const WS_URL = "ws://localhost:8000/ws";
+
+export function useLiveEvents() {
+  const [latestEvent, setLatestEvent] = useState(null);
+
+  useEffect(() => {
+    const ws = WebSocket(WS_URL);
+
+    ws.onmessage = (e) => {
+      try {
+        setLatestEvent(JSON.parse(e.data));
+      } catch (error) {
+        console.warn("Non-JSON WebSocket message:", e.data);
+      }
+    };
+
+    ws.onclose = () => console.log("WebSocket Closed");
+
+    return () => ws.close(); // cleanup environment
+  }, []);
+
+  return latestEvent;
+}
