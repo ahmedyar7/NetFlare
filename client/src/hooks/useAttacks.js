acc4069a52228c9ebc6bc5c92b8b2fefa7e05a3c
ahmedyar7 <ahmedyar778@gmail.com>
2026-07-14 18:46:46 +0500

    feat: Adding hook that would call the localhost:8000/attacks to get the lat and long of the attacks

---
+++ client/src/hooks/useAttacks.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+// FastAPI Endpoint
+const API = "http://localhost:8000";
+const jitter = () => (Math.random() - 0.5) * 0.8;
+
+export function useAttack() {
+  const [points, setPoints] = useState();
+
+  useEffect(() => {
+    fetch(`${API}/attacks`)
+      .then((res) => res.json())
+      .then((data) =>
+
+        setPoints(
+          data.map((d) => ({
+            ...d,
+            lat: d.lat + jitter(),
+            lng: d.lng + jitter(),
+          })),
+        ),
+      )
+
+      .catch((err) => console.error("Failed to load error: ", err));
+  }, []);
+
+
+  return points;
+}
