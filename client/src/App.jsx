b34bbe41f314aa3612b35c5c7b6e88649a693534
ahmedyar7 <ahmedyar778@gmail.com>
2026-07-14 20:04:12 +0500

    add: adding the feature the get the live attacks and the visulaize it using the rings and arcs live for better form of visualization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,64 @@
+import { useEffect, useState } from "react";
 import GlobeView from "./components/GlobeView";
 import { useAttack } from "./hooks/useAttacks";
 
-export default function App(){
-  
-  const points = useAttack();
+// hooks
+import { useAttack } from "./hooks/useAttacks";
+import { useLiveEvents } from "./hooks/useLiveEvents";
 
-  return <GlobeView points={points}/>;
+const jitter = () => (Math.random() - 0.5) * 0.8;
+
+export default function App() {
+  const initialPoints = useAttack();
+  const liveEvents = useLiveEvents();
+
+  const [livePoints, setLivePoints] = useState([]);
+  
+  const [rings, setRings] = useState([]);
+  const [arcs, setArcs] = useState([]);
+
+  useEffect(() => {
+    if (!liveEvents) {
+      return;
+    }
+
+    const point = {
+      ...liveEvents,
+      lat: liveEvents.lat + jitter(),
+      lng: liveEvents.lng + jitter(),
+    };
+
+    setLivePoints((prev) => [...prev, point]);
+
+    // Ripple ring at edge location
+    // removed after 3 second
+
+    const ring = {
+      lat: point.lat,
+      lng: point.lng,
+    };
+
+    setRings((prev) => [...prev, ring]);
+    setTimeout(() => setRings((prev) => prev.filter((r) => r != ring), 3000));
+
+    // arc form a random origin to the events after 4 second.
+
+    const arc = {
+      startLat: (Math.random() - 0.5) * 140,
+      startLng: (Math.random() - 0.5) * 360,
+      endLat: point.lat,
+      endLng: point.lng,
+    };
+
+    setArcs((prev) => [...prev, arc]);
+    setTimeout(() => setArcs((prev) => prev.filter((a) => a !== arc)), 4000);
+  }, [liveEvents]);
+
+  return (
+    <GlobeView
+      points={[...initialPoints, ...livePoints]}
+      rings={rings}
+      arcs={arcs}
+    />
+  );
 }
